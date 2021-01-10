77c3a1ca2722d320c6ec42557901e0fe66712073
kwstaspexli <kostaspechli2@gmail.com>
2021-01-10 21:41:28 +0200

    first commit

---
+++ _vti_cnf/d1341f9b7a.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TW|23 Feb 2020 09:16:44 -0000
+vti_author:SR|DESKTOP-IBJIM8T\\kwsta
+vti_modifiedby:SR|DESKTOP-IBJIM8T\\kwsta
+vti_nexttolasttimemodified:TW|23 Feb 2020 09:16:44 -0000
+vti_timecreated:TR|23 Feb 2020 10:21:24 -0000
+vti_cacheddtm:TX|23 Feb 2020 10:21:24 -0000
+vti_filesize:IR|9496
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|website2/index.html
